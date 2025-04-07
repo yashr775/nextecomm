0a7f4fde190c46ecdb24bf5fc452f20d479a6db8
yashr775 <rajy29629@gmail.com>
2025-04-07 21:01:53 +0530

    Pincode api created

---
+++ pages/api/pincode.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+
+    res.status(200).json([234400, 721308, 110003, 560017])
+
+}
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -4,6 +4,10 @@ import React from "react";
 const Post = () => {
     const router = useRouter();
     const { slug } = router.query;
+
+    const checkServicablity = () => { }
+
+
     return (
         <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-16 mx-auto">
@@ -177,6 +181,10 @@ const Post = () => {
                                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                 </svg>
                             </button>
+                            <div className="pin flex space-x-2 text-sm ml-2 ">
+                                <input type="text m-2 px-2 border-2  "></input>
+                                <button onClick={checkServicablity} className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Check</button>
+                            </div>
                         </div>
                     </div>
                 </div>
